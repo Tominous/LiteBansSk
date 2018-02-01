0c86cb172cbc55a9e540f2ee7c89b2d88d583e5f
Pugabyte <pugabear@bn-mc.net>
2018-02-01 13:02:38 -0500

    Small cleanup to exception handling, bump version

--- src/main/java/me/pugabyte/litebanssk/LiteBansSk.java
+++ src/main/java/me/pugabyte/litebanssk/LiteBansSk.java
@@ -7,11 +7,16 @@ import ch.njol.skript.classes.Parser;
 import ch.njol.skript.expressions.base.EventValueExpression;
 import ch.njol.skript.lang.ParseContext;
 import ch.njol.skript.registrations.Classes;
+import ch.njol.skript.util.Timespan;
+import ch.njol.skript.variables.Variables;
 import litebans.api.Entry;
 import litebans.api.Events;
 import me.pugabyte.litebanssk.skript.events.*;
+import org.bukkit.Bukkit;
+import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
 
+import java.awt.*;
 import java.io.IOException;
 
 public class LiteBansSk extends JavaPlugin {
@@ -44,25 +49,34 @@ public class LiteBansSk extends JavaPlugin {
 
 	@Override
 	public void onEnable() {
-		try {
-			String version = getInstance().getServer().getPluginManager().getPlugin("LiteBans").getDescription().getVersion();
+		String version = getInstance().getServer().getPluginManager().getPlugin("LiteBans").getDescription().getVersion();
 
-			if (version.startsWith("2.")) {
-				double versionDouble;
-				try {
-					versionDouble = Double.parseDouble(version.replaceFirst("2.", ""));
+		if (version.startsWith("2.")) {
+			double versionDouble;
+			try {
+				versionDouble = Double.parseDouble(version.replaceFirst("2.", ""));
 
-					// Most recent version I've built against
-					if (versionDouble < 2.5) {
-						getLogger().warning("LiteBans is not updated; some features may not work!");
-						getLogger().warning("Update: https://www.spigotmc.org/resources/3715/");
-					}
-				} catch (NumberFormatException e) {
-					getLogger().warning("Could not parse LiteBans version '" + version + "'");
+				// Most recent version I've built against
+				if (versionDouble < 2.5) {
+					getLogger().warning("LiteBans is not updated; some features may not work!");
+					getLogger().warning("Update: https://www.spigotmc.org/resources/3715/");
 				}
+			} catch (NumberFormatException e) {
+				getLogger().warning("Could not parse LiteBans version '" + version + "'");
 			}
+			Player player = Bukkit.getPlayer("Pugabyte");
+			Timespan hours = (Timespan) Variables.getVariable("hours::" + player.getUniqueId(), null, false);
+			long millis = Long.parseLong(hours.toString());
+			if (millis >= 3600000) {
+
+			}
+
+		}
+		try {
 			register();
-		} catch (IOException e) {
+		} catch (Exception e) {
+			getLogger().severe("Report this error to https://github.com/Pugabyte/LiteBansSk/issues");
+			getLogger().severe("Please include LiteBans, LiteBansSk, and Spigot versions");
 			e.printStackTrace();
 		}
 	}
@@ -73,32 +87,26 @@ public class LiteBansSk extends JavaPlugin {
 		Events.get().unregister(broadcastSent);
 	}
 
-	public void register() throws IOException {
+	public void register() throws Exception {
 		entryAdded = new Events.Listener() {
 			@Override
 			public void entryAdded(Entry entry) {
-				try {
-					if (entry.getType().equals("ban")) {
-						instance.getServer().getScheduler().runTaskLater(instance, () ->
-							instance.getServer().getPluginManager().callEvent(new BanEvent(entry))
-						, 0);
-					} else if (entry.getType().equals("mute")) {
-						instance.getServer().getScheduler().runTaskLater(instance, () ->
-							instance.getServer().getPluginManager().callEvent(new MuteEvent(entry))
-						, 0);
-					} else if (entry.getType().equals("kick")) {
-						instance.getServer().getScheduler().runTaskLater(instance, () ->
-							instance.getServer().getPluginManager().callEvent(new KickEvent(entry))
-						, 0);
-					} else if (entry.getType().equals("warn")) {
-						instance.getServer().getScheduler().runTaskLater(instance, () ->
-							instance.getServer().getPluginManager().callEvent(new WarnEvent(entry))
-						, 0);
-					}
-				} catch (Exception e) {
-					getLogger().severe("Report this error to https://github.com/Pugabyte/LiteBansSk/issues");
-					getLogger().severe("Please include LiteBans, LiteBansSk, and Spigot versions");
-					e.printStackTrace();
+				if (entry.getType().equals("ban")) {
+					instance.getServer().getScheduler().runTaskLater(instance, () ->
+						instance.getServer().getPluginManager().callEvent(new BanEvent(entry))
+					, 0);
+				} else if (entry.getType().equals("mute")) {
+					instance.getServer().getScheduler().runTaskLater(instance, () ->
+						instance.getServer().getPluginManager().callEvent(new MuteEvent(entry))
+					, 0);
+				} else if (entry.getType().equals("kick")) {
+					instance.getServer().getScheduler().runTaskLater(instance, () ->
+						instance.getServer().getPluginManager().callEvent(new KickEvent(entry))
+					, 0);
+				} else if (entry.getType().equals("warn")) {
+					instance.getServer().getScheduler().runTaskLater(instance, () ->
+						instance.getServer().getPluginManager().callEvent(new WarnEvent(entry))
+					, 0);
 				}
 			}
 		};
@@ -169,6 +177,6 @@ public class LiteBansSk extends JavaPlugin {
 					}
 				));
 
-		getAddonInstance().loadClasses("me.pugabyte.litebanssk", "skript");
+		getAddonInstance().loadClasses("me.pugabyte.litebanssk.skript");
 	}
 }
